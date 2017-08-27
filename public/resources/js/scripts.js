caf529547d692f1f198d7b7c7017beb64d197741
runnerdave <runnerdave@gmail.com>
2017-08-27 21:54:30 +1000

    all sections plus sticky navigation

---
+++ public/resources/js/scripts.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+
+    $('.js--section-about-me').waypoint(function (direction) {
+        if (direction == "down") {
+            $('nav').addClass('sticky');
+        } else {
+            $('nav').removeClass('sticky');
+        }
+    }, {
+        offset: '60px'
+    })
+});
