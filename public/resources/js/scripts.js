585f27d29ea96fb20b7ca9c57de4f5590873d252
runnerdave <runnerdave@gmail.com>
2017-08-29 19:37:24 +1000

    mobile nav and animations

--- public/resources/js/scripts.js
+++ public/resources/js/scripts.js
@@ -8,5 +8,68 @@ $(document).ready(function () {
         }
     }, {
         offset: '60px'
-    })
+    });
+
+    /*navigation scroll, taken from: https://css-tricks.com/snippets/jquery/smooth-scrolling/*/
+    $('a[href*="#"]')
+    // Remove links that don't actually link to anything
+        .not('[href="#"]')
+        .not('[href="#0"]')
+        .click(function (event) {
+            // On-page links
+            if (
+                location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+                &&
+                location.hostname == this.hostname
+            ) {
+                // Figure out element to scroll to
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                // Does a scroll target exist?
+                if (target.length) {
+                    // Only prevent default if animation is actually gonna happen
+                    event.preventDefault();
+                    $('html, body').animate({
+                        scrollTop: target.offset().top
+                    }, 1000, function () {
+                        // Callback after animation
+                        // Must change focus!
+                        var $target = $(target);
+                        $target.focus();
+                        if ($target.is(":focus")) { // Checking if the target was focused
+                            return false;
+                        } else {
+                            $target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
+                            $target.focus(); // Set focus again
+                        }
+                        ;
+                    });
+                }
+            }
+        });
+
+    $('.mobile-nav-icon').click(function () {
+        var nav = $('.main-nav');
+        var icon = $('.mobile-nav-icon i')
+        nav.slideToggle(200);
+        if (icon.hasClass('ion-navicon-round')) {
+            icon.removeClass('ion-navicon-round')
+            icon.addClass('ion-close-round');
+        } else {
+            icon.removeClass('ion-close-round')
+            icon.addClass('ion-navicon-round');
+        }
+    });
+
+    $('.js--melbourne').waypoint(function (direction) {
+        $('.js--melbourne').addClass('animated shake');
+    }, {
+        offset: '50%'
+    });
+
+    $('.js--active').waypoint(function (direction) {
+        $('.js--active').addClass('animated bounceInLeft');
+    }, {
+        offset: '50%'
+    });
 });
